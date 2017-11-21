76df61310bec49bee0dd38b5f2edfb6d296b60e8
HampeBea <Hampe_rock@hotmail.com>
2017-11-21 10:53:57 +0100

    Mada while Loop and somthing to start with
    -Array to get all the names

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sorting {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		int a;
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		while(true){
+			a=sc.nextInt();
+			
+			for(;a>0;a--){
+				list.add(sc.nextLine());
+			}
+			
+			for(int i = list.size()-1 ; i>= 0 ; i--){
+				
+				
+				
+				
+				
+			}
+			
+		}
+		
+
+	}
 
 }
